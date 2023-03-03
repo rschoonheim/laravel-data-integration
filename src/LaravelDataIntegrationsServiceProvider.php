de42c881b35794b3195f80d46a3bb225189522c5
Romano Schoonheim <rschoonheim96@gmail.com>
2023-03-03 21:31:31 +0100

    Git ignore..

---
+++ src/LaravelDataIntegrationsServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rschoonheim\LaravelDataIntegrations;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelDataIntegrationsServiceProvider extends ServiceProvider
+{
+
+}
